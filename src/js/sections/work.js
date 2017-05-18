7a180e0b641985976660511ad72ca5f8109eb04c
Evan McEldowney <evanmac123@gmail.com>
2017-05-18 08:50:06 -0400

    routing fixed

--- src/js/sections/work.js
+++ src/js/sections/work.js
@@ -2,6 +2,8 @@ import config from 'config'
 import utils from 'utils'
 import classes from 'dom-classes'
 import Default from './default'
+import { on, off } from 'dom-event'
+import framework from 'framework'
 
 class Work extends Default {
 
@@ -14,7 +16,7 @@ class Work extends Default {
 	}
 
 	init(req, done) {
-    this.route = req.params.id
+		this.route = req.params.id
 		super.init(req, done)
 	}
 
@@ -38,7 +40,6 @@ class Work extends Default {
 		tl.set(this.page, { scale: 1.1 })
 
 		tl.to(this.page, 2, { scale: 1, autoAlpha: 1 })
-		tl.staggerTo([...this.ui.content, ...this.ui.card], 1.2, { y: 0, autoAlpha: 1, clearProps: 'y' }, 0.15, '-=1.5')
 		tl.restart()
 	}
 
@@ -48,7 +49,6 @@ class Work extends Default {
 
 		const tl = new TimelineMax({ paused: true, onComplete: done })
 
-		tl.staggerTo([this.ui.card, this.ui.content], 1, { autoAlpha: 0 }, 0.2)
 		tl.to(this.page, 1, { autoAlpha: 0 })
 		tl.restart()
 	}
--- src/js/utils/biggie/model.js
+++ src/js/utils/biggie/model.js
@@ -3,7 +3,7 @@ export default (req, id) => {
   const path = id.split('/')
   const model = {}
 
- else if ( path.length = 2) {
+ if ( path.length = 2) {
 
     model['template'] = req.params.id ? 'work' : id
     model['data'] = req.params.id ? window._data.work[req.params.id] : window._data
