00f8f6b5c9c9cb3b8f27fe38deb3f9545b542dda
Evan McEldowney <evanmac123@gmail.com>
2017-05-25 15:22:10 -0400

    menu and about page routing

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,6 +2,7 @@ import config from 'config'
 
 module.exports = {
 	[`${config.BASE}`]: require('./sections/home'),
+	[`${config.BASE}about`]: { section: require('./sections/about') },
 	[`${config.BASE}work/:id`]: { section: require('./sections/work'), duplicate: true  }
 	}
 
